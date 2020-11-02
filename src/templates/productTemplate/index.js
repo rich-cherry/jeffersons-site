05628aa80a4b27ff447f6714630ceb6458167f94
rich-cherry <rich.m.cherry@gmail.com>
2020-11-02 15:35:27 -0500

    finally got price working

--- src/templates/productTemplate/index.js
+++ src/templates/productTemplate/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 import { Layout, ImageGallery } from 'components'
-import { Grid } from './styles'
+import { Grid, SelectWrapper, Price } from './styles'
+import CartContext from 'context/CartContext';
 
 export const query = graphql`
     query ProductQuery($shopifyId: String){
         shopifyProduct(shopifyId: {eq: $shopifyId}) {
-            title
+            shopifyId
+            title 
             description
             images {
                 id
@@ -23,12 +25,42 @@ export const query = graphql`
 `
 
 export default function ProductTemplate(props) {
+    const { getProductById } = React.useContext(CartContext);
+    const [product, setProduct] = React.useState(null);
+    const [selectedVariant, setSelectedVariant] = React.useState(null);
+    React.useEffect(() => {
+        getProductById(props.data.shopifyProduct.shopifyId).then((result) => {
+            setProduct(result);
+            setSelectedVariant(result.variants[0]);
+        })
+    }, [getProductById, setProduct, props.data.shopifyProduct.shopifyId])
+
+    const handleVariantChange = e => {
+        setSelectedVariant(product?.variants.find(v => v.id === e.target.value))
+    };
+
     return (
         <Layout>
             <Grid>
                 <div>
                     <h1>{props.data.shopifyProduct.title}</h1>
                     <p>{props.data.shopifyProduct.description}</p>
+                    {product?.availableForSale && (
+                        <>
+                            <SelectWrapper>
+                                <strong>Variant</strong>
+                                <select onChange={handleVariantChange}>
+                                    {product.variants.map(v => (
+                                        <option key={v.id} value={v.id}>
+                                            {v.title}
+                                        </option>
+                                    ))}
+                                </select>
+                            </SelectWrapper>
+                            {!!selectedVariant &&
+                                <Price>{selectedVariant.price}</Price>}
+                        </>
+                    )}
                 </div>
                 <div>
                     <ImageGallery images={props.data.shopifyProduct.images} />
@@ -36,5 +68,4 @@ export default function ProductTemplate(props) {
             </Grid>
         </Layout>
     )
-
 }
