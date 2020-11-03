ac6c06a182a885c79db26e9c20f3df62c7f77ba5
rich-cherry <rich.m.cherry@gmail.com>
2020-11-03 10:53:05 -0500

    fixed broken about page

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Layout, SEO } from 'components';
+
+const AboutPage = () => (
+    <Layout>
+        <SEO title="About" />
+        <h1>Coming Soon</h1>
+    </Layout>
+);
+
+export default AboutPage;
